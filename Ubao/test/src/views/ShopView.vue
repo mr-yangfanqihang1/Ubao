689643a7cc57bfa887047acb677c309052e789f0
qiaqiaaoligei <3525017966@qq.com>
2024-09-06 00:07:33 +0800

    new shop

--- Ubao/test/src/views/ShopView.vue
+++ Ubao/test/src/views/ShopView.vue
@@ -1,8 +1,5 @@
 <template>
   <el-container style="height: 100%; background-color: aliceblue; line-height: 1; border: 2px solid #eee">
-    <!-- <el-header style="display: flex; align-items: center; justify-content: center; height: 80px;background-color: rgb(238, 241, 246)">
-      我的数据库
-    </el-header> -->
     <el-container>
       <el-aside width="200px" style="height:600px; background-color: rgb(238, 241, 246)">
         <el-menu :default-openeds="['1', '3']" style="background-color: rgb(238, 241, 246)" active-background-color="#409eff" active-text-color="#409eff">
@@ -23,32 +20,21 @@
           <!-- 首页的具体内容 -->
         </div>
         <div v-if="currentView === 'goods'">
-          <h2>商品管理内容</h2>
-          <!-- 商品管理的具体内容 -->
+          <goods-management></goods-management> <!-- 使用商品管理组件 -->
         </div>
         <div v-if="currentView === 'order'">
-    
-            <el-dropdown trigger="click">
-            <span class="el-dropdown-link" style="cursor: pointer;">
-              <i class="el-icon-setting" style="margin-right: 15px;"></i>
-              设置
-            </span>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>查看</el-dropdown-item>
-              <el-dropdown-item>新增</el-dropdown-item>
-              <el-dropdown-item>删除</el-dropdown-item>
-            </el-dropdown-menu>
-          </el-dropdown>
-
+          <h2>订单管理内容</h2>
+          <!-- 订单管理内容 -->
           <el-form :inline="true" :model="formInline" class="demo-form-inline">
             <el-form-item label="订单编号">
               <el-input v-model="formInline.order_id" placeholder="请输入"></el-input>
             </el-form-item>
             <el-form-item label="订单状态">
-              <el-select v-model="formInline.region" placeholder="活动区域">
+              <el-select v-model="formInline.status" placeholder="全部">
+                <el-option label="全部" value="9" style="color:gray"></el-option>
                 <el-option label="未发货" value="1" style="color:red"></el-option>
-                <el-option label="已发货" value="2" style="color:green"></el-option>
-                <el-option label="已" value="2" style="color:green"></el-option>
+                <el-option label="已发货" value="2" style="color:orange"></el-option>
+                <el-option label="已签收" value="3" style="color:green"></el-option>
               </el-select>
             </el-form-item>
             <el-form-item>
@@ -56,12 +42,24 @@
             </el-form-item>
           </el-form>
           <table border="1" style="width: 100%; border-collapse: collapse; border: 1px solid rgb(235, 235, 235); text-align: left;">
-
             <thead>
               <tr>
-                <th style="padding: 8px;border: 1px  solid rgb(235, 235, 235);">日期</th>
-                <th style="padding: 8px;border: 1px  solid rgb(235, 235, 235)">姓名</th>
-                <th style="padding: 8px;border: 1px  solid rgb(235, 235, 235)">地址</th>
+                <th style="padding: 8px;border: 1px  solid rgb(235, 235, 235);">订单编号</th>
+                <th style="padding: 8px;border: 1px  solid rgb(235, 235, 235)">商品名称</th>
+                <th style="padding: 8px;border: 1px  solid rgb(235, 235, 235)">用户地址</th>
+                <th style="padding: 8px;border: 1px  solid rgb(235, 235, 235)">订单状态</th>
+                <th>
+                  <el-dropdown trigger="click">
+                    <span class="el-dropdown-link" style="cursor: pointer;">
+                      <i class="el-icon-setting" style="margin-right: 15px;"></i>
+                      设置
+                    </span>
+                    <el-dropdown-menu slot="dropdown">
+                      <el-dropdown-item>新增</el-dropdown-item>
+                      <el-dropdown-item>删除</el-dropdown-item>
+                    </el-dropdown-menu>
+                  </el-dropdown>
+                </th>
               </tr>
             </thead>
             <tbody>
@@ -71,7 +69,7 @@
                 <td style="padding: 8px;border: 1px  solid rgb(235, 235, 235)">{{ item.address }}</td>
               </tr>
             </tbody>
-            </table>
+          </table>
         </div>
       </el-main>
     </el-container>
@@ -80,7 +78,12 @@
 
 <script>
 import axios from 'axios';
+import GoodsManagement from './GoodsManagement.vue'; // 引入商品管理组件
+
 export default {
+  components: {
+    GoodsManagement // 注册商品管理组件
+  },
   data() {
     return {
       formInline: {
@@ -88,7 +91,7 @@ export default {
         status: ''
       },
       tableData: [],
-      currentView: 'order' // 默认显示首页内容
+      currentView: 'main' // 默认显示首页内容
     };
   },
   methods: {
@@ -97,10 +100,8 @@ export default {
     }
   },
   mounted() {
-    // 调整路径，确保路径正确
     axios.get("https://apifoxmock.com/m2/5075036-4736682-default/210094948")
       .then((result) => {
-        // 确保数据结构符合预期
         if (result.data.formInline) {
           this.formInline = result.data.formInline;
         } else {
@@ -116,3 +117,4 @@ export default {
   }
 };
 </script>
+
